c54dfc4409268617d855e0e3fab5a460c1b811a4
Michael <michaelhuytran@gmail.com>
2016-11-11 10:17:49 -0800

    [feature] - added input fields and button

---
+++ src/app/input-box/input-box.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-box',
+  templateUrl: './input-box.component.html',
+  styleUrls: ['./input-box.component.css']
+})
+export class InputBoxComponent {
+
+  constructor() { }
+
+
+
+}
---
+++ src/server/server.js
@@ -0,0 +1,3 @@
+/**
+ * Created by MikeTran on 11/11/16.
+ */
